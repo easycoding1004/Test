c69e42b66dbcfe0415ffaa3b84839fba3954e52c
easycoding1004 <easycoding1004@gmail.com>
2024-06-20 14:14:34 +0900

    T

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print('hello')
+t=int(input())
+garo=0
+garo_s=[]
+while t!=0:
+    s=input()
+    target=input()
+    stemp=0
+    for ch  in s:
+        if ch=='(':
+            garo+=1
+        else:
+            if garo==0:
+                stemp=1
+            else:
+
+    t-=1
